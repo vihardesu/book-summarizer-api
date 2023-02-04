326f5680b7e731e4fb42554ee94f7184a497532f
Vihar Desu <vihar.desu@tempus.com>
2023-02-04 00:43:51 -0500

    remove logger

--- src/main.ts
+++ src/main.ts
@@ -16,34 +16,6 @@ import { Queue } from 'bull';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // app.useLogger(
-  //   WinstonModule.createLogger({
-  //     format: winston.format.uncolorize(),
-  //     transports: [
-  //       new winston.transports.Console({
-  //         format: winston.format.combine(
-  //           winston.format.timestamp(),
-  //           winston.format.ms(),
-  //           nestWinstonModuleUtilities.format.nestLike(),
-  //         ),
-  //       }),
-  //       new WinstonCloudWatch({
-  //         name: 'Cloudwatch Logs',
-  //         logGroupName: process.env.CLOUDWATCH_GROUP_NAME,
-  //         logStreamName: process.env.CLOUDWATCH_STREAM_NAME,
-  //         awsAccessKeyId: process.env.AWS_ACCESS_KEY,
-  //         awsSecretKey: process.env.AWS_KEY_SECRET,
-  //         awsRegion: process.env.CLOUDWATCH_REGION,
-  //         messageFormatter: function (item) {
-  //           return (
-  //             item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-  //           );
-  //         },
-  //       }),
-  //     ],
-  //   }),
-  // );
-
   const serverAdapter = new ExpressAdapter();
   serverAdapter.setBasePath('/bull-board');
 
